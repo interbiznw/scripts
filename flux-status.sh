83f7f5d475c28569328dea6fd85cc77d274a3009
J62 <j.owens@interbiznw.com>
2022-03-23 04:10:09 -0700

    Update flux-status.sh

--- flux-status.sh
+++ flux-status.sh
@@ -5,6 +5,12 @@ echo "adding aliases to profile....."
 echo "alias toolbox='bash -i <(curl -s https://raw.githubusercontent.com/RunOnFlux/fluxnode-multitool/master/multitoolbox.sh)'" >> ~/.profile
 echo "alias status='flux-cli getzelnodestatus'" >> ~/.profile
 echo "alias benchmarks='flux-cli getbenchmarks'" >> ~/.profile
+echo "alias restart_zelcash='sudo systemctl restart zelcash'" >> ~/.profile
+echo "alias restartbenchmarks='fluxbench-cli restartnodebenchmarks'" >> ~/.profile
+echo "alias watch_logs='pm2 monit'" >> ~/.profile
+echo "alias restart_watchdog='pm2 reload watchdog --watch'" >> ~/.profile
+
+
 
 #add cmds to launch on ssh login for status update
 echo "adding launch status commands..."
