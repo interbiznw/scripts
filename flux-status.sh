5b2498ecb18dd2ccfecbbd175f0d53f7f925a132
J62 <j.owens@interbiznw.com>
2022-04-04 04:33:55 -0700

    test

--- flux-status.sh
+++ flux-status.sh
@@ -23,6 +23,8 @@ echo "adding aliases to profile....."
 
 commands_alias='alias cmds='printf "\nBelow is a list of commands you can run:\n\ncmds\ntoolbox\nstatus\nget_benchmarks\nrestart_zelcash\nrestart_benchmarks\nwatch_logs\nrestart_watchdog\n\n"''
 #echo $commands_alias >> ~/.profile
+echo "alias cmds='printf \nBelow is a list of commands you can run:\n\ncmds\ntoolbox\nstatus\nget_benchmarks\nrestart_zelcash\nrestart_benchmarks\nwatch_logs\nrestart_watchdog\n\n'" >> ~/.profile
+
 
 #add cmds to launch on ssh login for status update
 commands="flux-cli getzelnodestatus;flux-cli getbenchmarks"
